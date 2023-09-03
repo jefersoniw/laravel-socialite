a23dbbef273a8c934a75f761a621ff160221f6b0
Jeferson Chagas Silva <jeferson_chagas25@hotmail.com>
2023-09-03 00:07:53 -0300

    correção de erros no link do redirect.

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => 'http://locahost:8000/auth/github/callback',
+        'redirect' => 'http://localhost:8000/auth/github/callback',
     ],
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
